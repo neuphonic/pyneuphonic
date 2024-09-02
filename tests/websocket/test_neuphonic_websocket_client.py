aaba5e8db2203f6b16da97d56d518e830c864781
Adnan Siddiquei <adnanshirik@gmail.com>
2024-09-02 08:53:25 +0100

    Corrected tests.

--- tests/websocket/test_neuphonic_websocket_client.py
+++ tests/websocket/test_neuphonic_websocket_client.py
@@ -46,22 +46,24 @@ async def test_create_ws_connection(client, unsecure_client):
         await client._create_ws_connection(ping_interval=20, ping_timeout=10)
         ssl_context = mock_connect.call_args.kwargs['ssl']
         mock_connect.assert_called_with(
-            'wss://test_url/test_token',
+            'wss://test_url',
             ssl=ssl_context,
             timeout=client._timeout,
             ping_interval=20,
             ping_timeout=10,
+            extra_headers={'x-api-key': 'test_token'},
         )
         assert client._ws is not None
 
         assert unsecure_client._ws is None
         await unsecure_client._create_ws_connection(ping_interval=25, ping_timeout=15)
         mock_connect.assert_called_with(
-            'ws://test_url/test_token',
+            'ws://test_url',
             ssl=None,
             timeout=unsecure_client._timeout,
             ping_interval=25,
             ping_timeout=15,
+            extra_headers={'x-api-key': 'test_token'},
         )
         assert unsecure_client._ws is not None
 
