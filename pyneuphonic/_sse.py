bc152ebe94ae546a320eae530a1909754ee7c9ed
Adnan Siddiquei <adnanshirik@gmail.com>
2024-10-18 16:48:36 +0100

    Added some more typing and docs.

--- pyneuphonic/_sse.py
+++ pyneuphonic/_sse.py
@@ -1,12 +1,29 @@
 import httpx
 import json
+from typing import Generator
 
 from pyneuphonic._endpoint import Endpoint
-from pyneuphonic.models import TTSConfig, SSERequest, SSEResponse, to_dict
+from pyneuphonic.models import TTSConfig, SSEResponse, to_dict
 
 
 class SSEClient(Endpoint):
-    def jwt_auth(self):
+    def jwt_auth(self) -> None:
+        """
+        Authenticate with the server to obtain a JWT token.
+
+        This method sends a POST request to the server's authentication endpoint to exchange the
+        API key for a JWT token.
+        The token is then added to the headers for subsequent requests.
+        Using JWT auth for subsequent requests speeds up the time to first audio byte when using
+        the SSEClient.send method.
+        Using JWT auth is recommended for situations where latency is a priority.
+
+        Raises
+        ------
+        httpx.HTTPStatusError
+            If the authentication request fails, an HTTPStatusError is raised with
+            details about the failure.
+        """
         response = httpx.post(
             f'{self.http_url}/sse/auth', headers=self.headers, timeout=self.timeout
         )
@@ -23,7 +40,7 @@ class SSEClient(Endpoint):
         self.headers['Authorization'] = f'Bearer: {jwt_token}'
 
     @staticmethod
-    def _parse_sse_message(message: str) -> SSERequest:
+    def _parse_sse_message(message: str) -> SSEResponse:
         # Split the message into lines
         lines = message.split('\n')
         data = {}
@@ -45,7 +62,25 @@ class SSEClient(Endpoint):
 
         return None
 
-    def send(self, text: str, tts_config: TTSConfig | dict = TTSConfig()):
+    def send(
+        self, text: str, tts_config: TTSConfig | dict = TTSConfig()
+    ) -> Generator[SSEResponse, None, None]:
+        """
+        Send a text to the TTS (text-to-speech) service and receive a stream of SSEResponse messages.
+
+        Parameters
+        ----------
+        text : str
+            The text to be converted to speech.
+        tts_config : TTSConfig | dict, optional
+            The TTS configuration settings. Can be an instance of TTSConfig or a dictionary which
+            will be parsed into a TTSConfig.
+
+        Yields
+        ------
+        Generator[SSEResponse, None, None]
+            A generator yielding SSEResponse messages.
+        """
         if not isinstance(tts_config, TTSConfig):
             tts_config = TTSConfig(**tts_config)
 
