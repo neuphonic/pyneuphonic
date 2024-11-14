4bd32315cbd25ce1589b949fd4b8b8b464fcdcde
Adnan Siddiquei <adnanshirik@gmail.com>
2024-11-14 12:14:25 +0000

    Added a save_audio function and docs on how to do this.

---
+++ snippets/sse/save_audio.py
@@ -0,0 +1,19 @@
+from pyneuphonic import Neuphonic, save_audio
+import os
+
+
+def main():
+    client = Neuphonic(api_key=os.environ.get('NEUPHONIC_API_TOKEN'))
+    sse = client.tts.SSEClient()
+
+    response = sse.send('Hello, world! This is an example of saving audio to a file.')
+    audio_bytes = bytearray()
+
+    for item in response:
+        audio_bytes += item.data.audio
+
+    save_audio(audio_bytes=audio_bytes)
+
+
+if __name__ == '__main__':
+    main()
---
+++ snippets/websocket/save_audio.py
@@ -0,0 +1,32 @@
+from pyneuphonic import Neuphonic, WebsocketEvents, save_audio
+from pyneuphonic.models import WebsocketResponse
+import os
+import asyncio
+
+
+async def main():
+    client = Neuphonic(api_key=os.environ.get('NEUPHONIC_API_TOKEN'))
+
+    ws = client.tts.AsyncWebsocketClient()
+    audio_bytes = bytearray()
+
+    # Attach event handlers. Check WebsocketEvents enum for all valid events.
+    async def on_message(message: WebsocketResponse):
+        # add the audio we recieve to the bytearray so we can save it later
+        nonlocal audio_bytes
+        audio_bytes += message.data.audio
+
+    ws.on(WebsocketEvents.MESSAGE, on_message)  # attach the event handler
+
+    await ws.open()
+    await ws.send(
+        'Hello, world! This is an example of saving audio to a file.', autocomplete=True
+    )
+    await asyncio.sleep(3)  # wait until we recieve all of the messages from the server
+    await ws.close()  # close the websocket
+
+    # save audio to a file
+    save_audio(audio_bytes=audio_bytes)
+
+
+asyncio.run(main())
