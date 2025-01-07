0a3080d33921ebf042e37328c505cff088f45501
Adnan Siddiquei <adnanshirik@gmail.com>
2025-01-07 14:31:23 +0000

    Made the pyaudio warning a little bit more verbose.

--- pyneuphonic/agents.py
+++ pyneuphonic/agents.py
@@ -3,11 +3,7 @@ import aioconsole
 
 from pyneuphonic.client import Neuphonic
 from pyneuphonic.models import APIResponse, AgentResponse, AgentConfig, WebsocketEvents
-
-try:
-    from pyneuphonic.player import AsyncAudioPlayer, AsyncAudioRecorder
-except Exception as e:
-    print('WARNING: audio player and recorder not imported!')
+from pyneuphonic.player import AsyncAudioPlayer, AsyncAudioRecorder
 
 
 def default_on_message(message: APIResponse[AgentResponse]):
